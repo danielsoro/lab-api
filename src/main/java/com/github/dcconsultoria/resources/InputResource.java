fc14e08139ddc784e17aaf051f36aa3d7d881f42
Daniel "soro" Cunha <danielsoro@gmail.com>
2020-02-08 02:42:09 -0300

    Fixing LICENS and Javadoc authors

--- src/main/java/com/github/dcconsultoria/resources/InputResource.java
+++ src/main/java/com/github/dcconsultoria/resources/InputResource.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2020 DC Consultoria
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.github.dcconsultoria.resources;
 
 import com.github.dcconsultoria.domain.Input;
@@ -9,6 +25,9 @@ import java.util.List;
 
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 
+/**
+ * @author <a href="mailto:danielsoro@apache.org">Daniel "soro" Cunha</a>
+ */
 @Path("/inputs")
 @Consumes(APPLICATION_JSON)
 @Produces(APPLICATION_JSON)
--- src/test/java/com/github/dcconsultoria/resources/InputResourceTest.java
+++ src/test/java/com/github/dcconsultoria/resources/InputResourceTest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2020 DC Consultoria
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.github.dcconsultoria.resources;
 
 import io.quarkus.test.junit.QuarkusTest;
@@ -7,6 +23,9 @@ import static io.restassured.RestAssured.given;
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 import static org.hamcrest.Matchers.is;
 
+/**
+ * @author <a href="mailto:danielsoro@apache.org">Daniel "soro" Cunha</a>
+ */
 @QuarkusTest
 public class InputResourceTest {
 
